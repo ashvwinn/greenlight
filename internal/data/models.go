d6755623ab6f13ef6be07ca8e0a19ebbe51c8278
ashvin <ashvinjangid10@gmail.com>
2025-05-27 11:19:43 +0530

    setup user model with CRUD operations

--- internal/data/models.go
+++ internal/data/models.go
@@ -11,11 +11,11 @@ var (
 )
 
 type Models struct {
-	Movie MovieModel
+	User   UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movie: MovieModel{DB: db},
+		User:   UserModel{DB: db},
 	}
 }
